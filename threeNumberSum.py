c203b459e86d3909f4a8b369ae88eea2c36400b2
ThisChicanery <jonathandrko@gmail.com>
2022-10-08 23:09:54 +0100

    working solution

--- threeNumberSum.py
+++ threeNumberSum.py
@@ -1,21 +1,31 @@
 def threeNumberSum(array, targetSum):
     array.sort()
-    left = 0
-    right = len(array)-1
     newarr = []
-    for i in array:
- 
-        currentSum = i + array[left] + array[right]
 
-        while left < right:
-            if currentSum == targetSum:
-                newarr.append([i,array[left],array[right]])
-            elif currentSum > targetSum:
-                right -= 1
-            else:
+    for i in range(len(array)-2):
+        left = i + 1
+        right = len(array)-1
+        print('this bit works')
+
+        while left < right:        
+            cs = array[i] + array[left] + array[right]
+
+            if cs == targetSum:
+                newarr.append([array[i], array[left], array[right]])
                 left += 1
-            
-            
-        
+                right -= 1
+            elif cs < targetSum:
+                left += 1
+            else:
+                right -= 1
+                
 
+                
     return newarr
+
+
+
+# x= [12, 3, 1, 2, -6, 5, -8, 6]
+# y =0
+
+# print(threeNumberSum(x,y))
