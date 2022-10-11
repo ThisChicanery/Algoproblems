942e7b014c17d6324ea0f4deecbd0a579cdd888c
ThisChicanery <jonathandrko@gmail.com>
2022-10-11 23:51:52 +0100

    first sorting algorithm

---
+++ bubbleSort.py
@@ -0,0 +1,14 @@
+def bubbleSort(array):
+    isSorted = False
+
+    while not isSorted:
+        isSorted = True
+        for i in range(len(array) -1):
+            if array[i] > array[i+1]:
+                array[i],array[i+1] = array[i+1],array[i]
+                isSorted = False
+
+    return array
+
+x = [8, 5, 2, 9, 5, 6, 3]
+print(bubbleSort(x))
